fd4f8dd0c0ca99fedcf61567c9f5dbb8af227fbd
winwinds <562258693@qq.com>
2020-02-20 01:18:11 +0800

    feature: 添加切换语言功能（缺陷：需要刷新页面）

--- packages/button/src/button.vue
+++ packages/button/src/button.vue
@@ -1,13 +1,14 @@
 <template>
-  <button class="so-btn_primary" style="color:orange">{{test}} {{t('el.test.name')}}</button>
-  <!-- <button style="color:orange">{{ t('el.test.name') }}</button> -->
+  <!-- <button class="so-btn_primary" style="color:orange">{{test}} {{t('el.test.name')}}</button> -->
+  <button class="so-btn_primary" style="color:orange">{{ t('el.test.name') }}</button>
 </template>
 <script type="text/babel">
-import { t } from "../../../src/locale";
+// import { t } from "../../../src/locale";
 // import Locale from 'element-ui/src/mixins/locale';
 import Locale from "../../../src/mixins/locale";
 
 export default {
+  //mixins用来挂载t(翻译)函数
   mixins: [Locale],
   name: "SoButton",
   data() {
@@ -16,9 +17,9 @@ export default {
     };
   },
   computed: {
-    test() {
-      return t("el.test.name");
-    }
+    // test() {
+    //   return t("el.test.name");
+    // }
   }
 };
 </script>
--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,51 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
     A UI FrameWork!!!
+    <button @click="setLang('zh-CN')">中文</button>
+    <button @click="setLang('en')">英文</button>
     <so-button></so-button>
     <so-list v-for="(item,index) in 3" :key="index"></so-list>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-// import RButton from '../node_modules/rainbow-candy/package'
+import { use } from "main/locale";
+import zhLocale from "main/locale/lang/zh-CN";
+import enLocale from "main/locale/lang/en";
+
+const LANGUAGE_KID = ["zh-CN", "en"]; //预设语言种类
+
+// localStorage.setItem("SOROSIS_LANGUAGE", "en");
+
+const localize = lang => {
+  //只是单纯的设置语言，页面不会实时跟随变化。
+  //解决方案，设置个current lang = zhLocale | enLocale,然后显示的内容用computed来做，相当于做了个变化检测（观察者模式）
+  switch (lang) {
+    case "zh-CN":
+      use(zhLocale);
+      break;
+    default:
+      console.log("执行");
+      use(enLocale);
+  }
+};
+localize(localStorage.getItem("SOROSIS_LANGUAGE"));
+
 export default {
   name: "app",
   components: {},
-  created: function() {}
+  created: function() {},
+  methods: {
+    setLang(lang) {
+      // console.log("执行设置语言");
+      // alert("设置成功，请刷新页面");
+
+      localStorage.setItem("SOROSIS_LANGUAGE", lang);
+      alert("切换语言成功！！");
+      location.reload();
+      // localize(localStorage.getItem("SOROSIS_LANGUAGE"));
+    }
+  }
 };
 </script>
 
